8596ed0bd69fa80ed544549081597a47ce352ae4
Kevin-Gottfried <44210078+Kevin-Gottfried@users.noreply.github.com>
2018-12-10 15:03:22 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 class Main {
     
-    float len;
-    float width;
-    float ht;
-    float vol;
-    float Surfa2;
-    float base;
-    float s1;
-    float s2;
+    float l;        //length variable
+    float w;        //width variable
+    float h;        //height variable
+    float v;        //volume variable
+    float a;        //surface area variable
+    float b;        //base variable, in triangles
+    float s1;       //variable for side one, in square pyramids
+    float s2;       //variable for side two, in square pyramids
 
+  public double findVol(double v) {
+    return len*width*ht;
+  }
+  public double findSurfArea(double a) {
+    return 2*l*w+
+  }
   public static char ChooseShape() {
     Scanner Reader = new Scanner(System.in);
     System.out.print("What is the shape you want? \n[a] Rectangular Prism? \n[b] Cube? \n[c] Square Pyramid? \n[d] Triangular Prism?");
@@ -17,20 +23,20 @@ class Main {
     char Shape = Reader.next().charAt(0);
     System.out.println(Shape);
       if (Shape == 'a' || Shape == 'b' || Shape =='c' || Shape == 'd'){
-        if (Shape == 'a') {
+        if (Shape == 'a') {   
           // Main.areaCalc() {}
         }
         else if (Shape == 'b') {
           // Cube.areaCalc() {}
         }
         else if (Shape == "c") {
-
+          
         }
         else if (Shape == 'd') {
 
         }
         else {
-          System.out.println("Please choose one of the letters assigned to the shapes");
+          System.out.println("Please choose a shape representing letter!");
         }
         /*
         branching for different shapes will be programmed here */
@@ -49,4 +55,3 @@ class Main {
 
   }
 }
-This is a change
